58964724fd5e394dbeb5327f68563d0ab6b41813
Eugen Kiss <eugen@eugenkiss.com>
2015-03-09 22:47:56 +0100

    Fix Util.drawableToBitmap IllegalArgumentException. fixes #129

--- src/main/java/anabolicandroids/chanobol/util/Util.java
+++ src/main/java/anabolicandroids/chanobol/util/Util.java
@@ -227,10 +227,14 @@ public class Util {
     }
 
     // http://stackoverflow.com/a/10600736/283607
-    public static Bitmap drawableToBitmap (Drawable drawable) {
+    public static Bitmap drawableToBitmap(Drawable drawable) {
         if (drawable instanceof BitmapDrawable) {
             return ((BitmapDrawable)drawable).getBitmap();
         }
+
+        int w = drawable.getIntrinsicWidth();
+        int h = drawable.getIntrinsicHeight();
+        if (w <= 0 || h <= 0) return null;
 
         Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(bitmap);
