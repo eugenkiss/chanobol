f8cfe2a22222e1a3674bb8b731aea9fc6cb6e42b
Eugen Kiss <eugen@eugenkiss.com>
2015-03-14 12:27:50 +0100

    Update to Ion 2.1.1
    Seems to be faster and better Gif handling

--- src/debug/java/anabolicandroids/chanobol/api/MockImageLoader.java
+++ src/debug/java/anabolicandroids/chanobol/api/MockImageLoader.java
@@ -12,6 +12,7 @@ import com.koushikdutta.async.http.cache.ResponseCacheMiddleware;
 import com.koushikdutta.async.http.callback.HttpConnectCallback;
 import com.koushikdutta.ion.HeadersResponse;
 import com.koushikdutta.ion.Ion;
+import com.koushikdutta.ion.ResponseServedFrom;
 import com.koushikdutta.ion.loader.SimpleLoader;
 
 import timber.log.Timber;
@@ -37,7 +38,7 @@ public class MockImageLoader extends SimpleLoader {
         return (Future<DataEmitter>)(Future)ion.getHttpClient().execute(request, new HttpConnectCallback() {
             @Override public void onConnectCompleted(Exception ex, AsyncHttpResponse response) {
                 long length = -1;
-                int loadedFrom = LoaderEmitter.LOADED_FROM_NETWORK;
+                ResponseServedFrom loadedFrom = ResponseServedFrom.LOADED_FROM_NETWORK;
                 HeadersResponse headers = null;
                 AsyncHttpRequest request = null;
                 if (response != null) {
@@ -46,9 +47,9 @@ public class MockImageLoader extends SimpleLoader {
                     length = HttpUtil.contentLength(headers.getHeaders());
                     String servedFrom = response.headers().get(ResponseCacheMiddleware.SERVED_FROM);
                     if (TextUtils.equals(servedFrom, ResponseCacheMiddleware.CACHE))
-                        loadedFrom = LoaderEmitter.LOADED_FROM_CACHE;
+                        loadedFrom = ResponseServedFrom.LOADED_FROM_CACHE;
                     else if (TextUtils.equals(servedFrom, ResponseCacheMiddleware.CONDITIONAL_CACHE))
-                        loadedFrom = LoaderEmitter.LOADED_FROM_CONDITIONAL_CACHE;
+                        loadedFrom = ResponseServedFrom.LOADED_FROM_CONDITIONAL_CACHE;
                 }
                 callback.onCompleted(ex,
                         new LoaderEmitter(response, length, loadedFrom, headers, request));
--- src/debug/java/anabolicandroids/chanobol/api/MockRestLoader.java
+++ src/debug/java/anabolicandroids/chanobol/api/MockRestLoader.java
@@ -12,6 +12,7 @@ import com.koushikdutta.async.http.cache.ResponseCacheMiddleware;
 import com.koushikdutta.async.http.callback.HttpConnectCallback;
 import com.koushikdutta.ion.HeadersResponse;
 import com.koushikdutta.ion.Ion;
+import com.koushikdutta.ion.ResponseServedFrom;
 import com.koushikdutta.ion.loader.SimpleLoader;
 
 import timber.log.Timber;
@@ -30,7 +31,7 @@ public class MockRestLoader extends SimpleLoader {
         return (Future<DataEmitter>)(Future)ion.getHttpClient().execute(request, new HttpConnectCallback() {
             @Override public void onConnectCompleted(Exception ex, AsyncHttpResponse response) {
                 long length = -1;
-                int loadedFrom = LoaderEmitter.LOADED_FROM_NETWORK;
+                ResponseServedFrom loadedFrom = ResponseServedFrom.LOADED_FROM_NETWORK;
                 HeadersResponse headers = null;
                 AsyncHttpRequest request = null;
                 if (response != null) {
@@ -39,9 +40,9 @@ public class MockRestLoader extends SimpleLoader {
                     length = HttpUtil.contentLength(headers.getHeaders());
                     String servedFrom = response.headers().get(ResponseCacheMiddleware.SERVED_FROM);
                     if (TextUtils.equals(servedFrom, ResponseCacheMiddleware.CACHE))
-                        loadedFrom = LoaderEmitter.LOADED_FROM_CACHE;
+                        loadedFrom = ResponseServedFrom.LOADED_FROM_CACHE;
                     else if (TextUtils.equals(servedFrom, ResponseCacheMiddleware.CONDITIONAL_CACHE))
-                        loadedFrom = LoaderEmitter.LOADED_FROM_CONDITIONAL_CACHE;
+                        loadedFrom = ResponseServedFrom.LOADED_FROM_CONDITIONAL_CACHE;
                 }
                 callback.onCompleted(ex,
                         new LoaderEmitter(response, length, loadedFrom, headers, request));
